b8fd2a523a3bb6cd32f5fdb262e62f7c926cd18b
Chintan Shah <chintan.shah@technostacks.com>
2021-07-18 14:01:39 +0530

    :tada: initial commit

---
+++ src/filters/all-exceptions.filter.ts
@@ -0,0 +1,44 @@
+import {
+  HttpStatus,
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+} from '@nestjs/common';
+import { Response as ExpressResponse } from 'express';
+import { ExceptionResponse } from '@interfaces/exception-response.interface';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
+
+@Catch()
+export default class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx: HttpArgumentsHost = host.switchToHttp();
+    const res = ctx.getResponse<ExpressResponse>();
+    const exceptionResponse: null | ExceptionResponse = exception.getResponse
+      ? (exception.getResponse() as ExceptionResponse)
+      : null;
+    const status: number = exception.getStatus ? exception.getStatus() : 500;
+
+    const mongodbCodes = {
+      bulkWriteError: 11000, // a duplicate error code in mongoose
+    };
+
+    if (exception.code === mongodbCodes.bulkWriteError) {
+      return res.status(HttpStatus.CONFLICT).json({
+        message: exceptionResponse?.message,
+        error: exceptionResponse?.error,
+      });
+    }
+
+    if (exception.response.error === 'ValidationError') {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        validationMessages: exceptionResponse?.message,
+        error: exceptionResponse?.error,
+      });
+    }
+
+    return res.status(status).json({
+      message: exceptionResponse?.message,
+      error: exceptionResponse?.error,
+    });
+  }
+}
